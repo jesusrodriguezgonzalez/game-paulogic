e1f9eb491ad1d93ecf91efaeb5ab243bed2c4351
Jesus Rodriguez <jesusrodriguezgonzalezgr@gmail.com>
2022-06-30 13:50:24 +0200

    Solved problem with deploy

--- src/components/InputText.jsx
+++ src/components/InputText.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { Context } from "../context/context-provider";
 
 export function Input() {
--- src/context/context-provider.js
+++ src/context/context-provider.js
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState } from "react";
 import Swal from "sweetalert2";
 
 export const Context = createContext({
